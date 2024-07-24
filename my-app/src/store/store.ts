913d1ad5abd7bd9a07c2d0f6e538fbac8789db4e
faridbayramov <f.bayramov@code.edu.az>
2024-07-24 12:20:01 +0400

    last edit

--- my-app/src/store/store.ts
+++ my-app/src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../store/authSlice';
+import authReducer from './authSlice';
 
 const store = configureStore({
   reducer: {
